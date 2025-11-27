4224b3d1940d7aa33db3d13acd815f39c96994eb
ikhlas <ikhlas2003@gmail.com>
2025-11-27 09:03:01 +0530

    Landing page have added steps

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import Header from "../components/header.jsx"
+import BgRemovalSteps from "../components/BgRemovalSteps.jsx"
+const Home = () => {
+    return (
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16 font-['Outfit']">
+            {/*Hero Section*/}
+            <Header/>
+            {/*Background removal steps section*/}
+            <BgRemovalSteps/>
+            {/*Background removal slider section*/}
+
+            {/*Buy credits plan section*/}
+
+            {/*User testimonials section*/}
+
+            {/*Try now*/}
+        </div>
+    )
+}
+
+export default Home;
