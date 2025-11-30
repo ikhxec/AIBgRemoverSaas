a9a0cf616aa473ffed901548ec7189c7ac1f9fc9
ikhlas <ikhlas2003@gmail.com>
2025-12-01 00:11:45 +0530

    manged to create the slider component and have added the styles

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import Header from "../components/header.jsx"
 import BgRemovalSteps from "../components/BgRemovalSteps.jsx"
+import BgSlider from "../components/BgSlider.jsx"
 const Home = () => {
     return (
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16 font-['Outfit']">
@@ -8,7 +9,7 @@ const Home = () => {
             {/*Background removal steps section*/}
             <BgRemovalSteps/>
             {/*Background removal slider section*/}
-
+            <BgSlider/>
             {/*Buy credits plan section*/}
 
             {/*User testimonials section*/}
